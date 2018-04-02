3e5a7312cf6dbf338070a95bfc4f5f36518ebf75
Jay Phillips <jayphillips@icloud.com>
2018-04-02 16:23:32 -0400

    first commit

---
+++ TRAILguide/Services/DataService.swift
@@ -0,0 +1,9 @@
+//
+//  DataService.swift
+//  TRAILguide
+//
+//  Created by Jay Phillips on 4/2/18.
+//  Copyright © 2018 Jay Phillips. All rights reserved.
+//
+
+import Foundation
---
+++ TRAILguide/View/CategoryCell.swift
@@ -0,0 +1,24 @@
+//
+//  CategoryCell.swift
+//  TRAILguide
+//
+//  Created by Jay Phillips on 4/2/18.
+//  Copyright © 2018 Jay Phillips. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
